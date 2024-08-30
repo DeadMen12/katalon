b27e7b324277cb17e7856cbfc6887f49d8a1208f
Your Name <your.email@example.com>
2024-08-30 10:06:36 +0700

    Add Transaksi On Branch

---
+++ Katalon Project/Belajar Katalon/Object Repository/Login Page/Button Login.rs
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Button Login</name>
+   <tag></tag>
+   <elementGuidId>29ed5f47-d529-4423-a157-83bd6b8224f0</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@type='submit']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=button[name=&quot;Enter to Login&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+      <webElementGuid>29a7fb93-2c3f-456e-a8e4-76c9b39b0496</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>type</name>
+      <type>Main</type>
+      <value>submit</value>
+      <webElementGuid>609d557c-9fbd-4f99-907e-f970ea007121</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>cursor-pointer rounded-md flex gap-1 items-center bg-[#5485BC]  hover:bg-[#6fa5e2] text-white text-[16px] font-medium px-4 rounded-md w-full justify-center py-3  false</value>
+      <webElementGuid>5db97623-572b-433c-b0ac-36fcf390a741</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Enter to Login</value>
+      <webElementGuid>728914fc-ea3c-40a4-be91-7b27793b12eb</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;__next&quot;)/div[2]/div[@class=&quot;flex justify-end md:justify-end items-center bg-cover bg-center global-bg&quot;]/div[@class=&quot;rounded-lg&quot;]/div[@class=&quot;bg-white p-10 rounded-lg w-[528px] mx-[73px] my-8 h-[650px] flex flex-col overflow-auto&quot;]/div[@class=&quot;flex-grow&quot;]/div[1]/form[1]/div[@class=&quot;flex flex-col gap-y-4 mt-3&quot;]/button[@class=&quot;cursor-pointer rounded-md flex gap-1 items-center bg-[#5485BC]  hover:bg-[#6fa5e2] text-white text-[16px] font-medium px-4 rounded-md w-full justify-center py-3  false&quot;]</value>
+      <webElementGuid>754e067c-c47f-4de0-a4a9-1a8e4cbe925a</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//button[@type='submit']</value>
+      <webElementGuid>f7974ab8-7751-4999-8f03-f02c5f9451a8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='__next']/div[2]/div/div/div/div[2]/div/form/div[2]/button</value>
+      <webElementGuid>1bdd3056-f22a-413b-aec3-ae34702e1592</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Password'])[1]/following::button[1]</value>
+      <webElementGuid>9ff84e2a-b9f1-4c6b-9710-059ee2cac2f8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Email Address'])[1]/following::button[1]</value>
+      <webElementGuid>4af32d47-1232-4449-b198-7e65033a7e9a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Click Here'])[1]/preceding::button[1]</value>
+      <webElementGuid>22b13e75-29e7-440d-aee4-7c2558833d72</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//button</value>
+      <webElementGuid>c55bb3c1-c7f0-4e40-b4df-cccfa10782eb</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//button[@type = 'submit' and (text() = 'Enter to Login' or . = 'Enter to Login')]</value>
+      <webElementGuid>63e58bf3-9f89-4136-ada2-6964e9f461d0</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
